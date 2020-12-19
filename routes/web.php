1a1c3a4134f62639c55946868b777f7c70593929
ShahadatPOM <naiem.3112@gmail.com>
2020-12-19 13:14:12 +0600

    admin template

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,8 @@ use Illuminate\Support\Facades\Route;
 // Auth
 Auth::routes();
 
+//back
+Route::get('/admin', 'Back\BackController@dashboard')->name('back.dashboard');
+
 // Front
 Route::get('/', 'Front\FrontController@index')->name('front.index');
